b37f7d99369f87974031ed9dd2f9f9cf0226a95d
hanna-ngo <154572191+hanna-ngo@users.noreply.github.com>
2024-01-23 11:36:45 -0500

    Update train.py

--- train.py
+++ train.py
@@ -3,3 +3,6 @@ from tqdm import tqdm
 from args import Arguments 
 from UNet import UNetVgg16
 from datasets import get_dataloaders 
+from eval import eval_epoch 
+from utils import AverageMeter, ScoreMeter, Recorder, ModelSaver, LRScheduler, get_optimizer, get_loss_fn 
+
